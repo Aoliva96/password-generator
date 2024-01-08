70aacfa1776409fd02d658a88201027cdfeaee33
Aster Oliva <asterthealabaster@gmail.com>
2024-01-07 18:49:54 -0800

    added variable declare to JS

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Password Generator
 
 ## A study in generating randomized passwords in JavaScript
+
+worked with keegan, george, and meera in a 1/7 study group.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,10 +7,125 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var numChoice = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var uppChoice = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowChoice = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var SpeChoice = [
+    " !",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+}
+
+// // Var declare
+// var numChoice = ["0123456789"];
+// var uppChoice = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+// var lowChoice = ["abcdefghijklmnopqrstuvwxyz"];
+// var SpeChoice = [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
+
+// var userChoice = "";
+
+// function generatePassword() {
+//   // Prompt user choice for numbers
+//   var numChoice = prompt(
+//     "Include numbers in your new password? \n('Y' for yes, 'N' for no)"
+//   );
+
+//   if (numChoice == "N") {
+//     confirm("You have chosen not to include numbers in your new password.");
+//   } else if (numChoice === false) {
+//     alert("Closing password generator, thank you.");
+//   } else {
+//     var userChoice = userChoice.push[i];
+//   }
+// }
